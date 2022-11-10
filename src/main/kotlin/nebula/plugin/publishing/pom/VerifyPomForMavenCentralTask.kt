ba1ba1e4e8f2d4f3ce74503b8007f27de704b159
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2022-11-09 19:09:28 -0800

    Add missing task annotations

--- src/main/kotlin/nebula/plugin/publishing/pom/VerifyPomForMavenCentralTask.kt
+++ src/main/kotlin/nebula/plugin/publishing/pom/VerifyPomForMavenCentralTask.kt
@@ -4,11 +4,16 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
+import org.gradle.work.DisableCachingByDefault
 import javax.inject.Inject
 
+@DisableCachingByDefault
 open class VerifyPomForMavenCentralTask @Inject constructor(objects: ObjectFactory): DefaultTask() {
     @InputFile
+    @PathSensitive(PathSensitivity.NONE)
     val pomFile: RegularFileProperty = objects.fileProperty()
 
     @TaskAction
