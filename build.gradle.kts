d70cb33af92cbf8b78517a3973690618f616c6d1
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2023-01-05 10:45:21 -0800

    plugin-plugin 20.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ import nebula.plugin.contacts.Contact
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
-    id("com.netflix.nebula.plugin-plugin") version "20.2.2"
+    id("com.netflix.nebula.plugin-plugin") version "20.3.0"
     kotlin("jvm") version "1.8.0"
 }
 
