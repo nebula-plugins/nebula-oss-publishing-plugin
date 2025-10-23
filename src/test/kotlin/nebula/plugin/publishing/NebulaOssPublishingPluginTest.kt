16919b55d00eac9d609240806254f86bc92872a5
John Burns <wakingrufus@gmail.com>
2025-10-23 16:27:48 -0500

    Modernize build and some tests

---
+++ src/test/kotlin/nebula/plugin/publishing/NebulaOssPublishingPluginTest.kt
@@ -0,0 +1,42 @@
+package nebula.plugin.publishing
+
+import nebula.test.dsl.TestKitAssertions.assertThat
+import nebula.test.dsl.plugins
+import nebula.test.dsl.rootProject
+import nebula.test.dsl.testProject
+import nebula.test.dsl.version
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+internal class NebulaOssPublishingPluginTest {
+    @TempDir
+    lateinit var projectDir: File
+
+    @Test
+    fun `Apply plugin without failures`() {
+        val runner = testProject(projectDir) {
+            rootProject {
+                plugins {
+                    id("java")
+                    id("com.netflix.nebula.maven-publish") version "19.0.0"
+                    id("com.netflix.nebula.oss-publishing")
+                }
+                rawBuildScript(
+                    """
+            group = "test"
+            nebulaOssPublishing {
+                netflixOssRepositoryBaseUrl = "http://"
+                netflixOssRepository = "my-releases"
+                netflixOssUsername = "user"
+                netflixOssPassword = "password"
+            }
+                """
+                )
+            }
+        }
+        val result = runner.run("build")
+        assertThat(result.task(":build")).hasOutcome(TaskOutcome.SUCCESS)
+    }
+}
---
+++ src/test/kotlin/nebula/plugin/publishing/NebulaSigningPluginTest.kt
@@ -0,0 +1,120 @@
+/**
+ *
+ *  Copyright 2021 Netflix, Inc.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ *
+ */
+package nebula.plugin.publishing
+
+import nebula.test.dsl.TestKitAssertions.assertThat
+import nebula.test.dsl.plugins
+import nebula.test.dsl.rootProject
+import nebula.test.dsl.testProject
+import nebula.test.dsl.version
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+internal class NebulaSigningPluginTest {
+    @TempDir
+    lateinit var projectDir: File
+
+    @Test
+    fun `register signing task`() {
+        val runner = testProject(projectDir) {
+            rootProject {
+                plugins {
+                    id("com.netflix.nebula.maven-publish") version ("19.0.0")
+                    java()
+                    id("com.netflix.nebula.signing")
+                }
+                rawBuildScript(
+                    """
+nebulaOssPublishing {
+    signingKey = "something"
+    signingPassword = "something"
+}
+tasks.named("publishNebulaPublicationToMavenLocal") { onlyIf { false }}
+afterEvaluate {
+    tasks.named("signNebulaPublication") { onlyIf { false }}
+}
+"""
+                )
+            }
+        }
+
+        val result = runner.run("publishNebulaPublicationToMavenLocal")
+        assertThat(result.task(":signNebulaPublication")).hasOutcome(TaskOutcome.SKIPPED)
+    }
+
+    @Test
+    fun `do not register signing task if key and password are not present`() {
+        val runner = testProject(projectDir) {
+            rootProject {
+                plugins {
+                    id("com.netflix.nebula.maven-publish") version ("19.0.0")
+                    java()
+                    id("com.netflix.nebula.signing")
+                }
+            }
+        }
+
+        val result = runner.run("publishNebulaPublicationToMavenLocal", "--dry-run")
+        assertThat(result.task(":signNebulaPublication")).isNull()
+    }
+
+    @Test
+    fun `only signs nebula publication`() {
+        val runner = testProject(projectDir) {
+            rootProject {
+                plugins {
+                    id("com.netflix.nebula.maven-publish") version ("19.0.0")
+                    java()
+                    id("com.netflix.nebula.signing")
+                }
+                rawBuildScript(
+                    """
+nebulaOssPublishing {
+    signingKey = "something"
+    signingPassword = "something"
+}
+publishing {
+    publications {
+        create<MavenPublication>("customMaven") {
+            groupId = "org.gradle.sample"
+            artifactId = "library"
+            version = "1.1"
+
+            from(components["java"])
+        }
+    }
+}
+tasks.named("publishCustomMavenPublicationToMavenLocal") { onlyIf { false }}
+tasks.named("publishNebulaPublicationToMavenLocal") { onlyIf { false }}
+afterEvaluate {
+    tasks.named("signNebulaPublication") { onlyIf { false }}
+}
+"""
+                )
+            }
+        }
+
+        val result =
+            runner.run("publishCustomMavenPublicationToMavenLocal", "publishNebulaPublicationToMavenLocal")
+
+        assertThat(result.task(":signNebulaPublication")).hasOutcome(TaskOutcome.SKIPPED)
+        assertThat(result.task(":signCustomMavenPublication")).isNull()
+    }
+}
